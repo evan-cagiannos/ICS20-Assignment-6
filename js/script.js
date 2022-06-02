5043ea6bfa9cbbdbf4c516ebe70e6d5d5b6e98b2
Evan Cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-06-02 16:35:46 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/* Created by: Evan Cagiannos
+   Created on: May 2022
+   This file contains the JS functions for index.html*/
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS20-Assignment-6/sw.js", {
+    scope: "/ICS20-Assignment-6/",
+  })
+}
+
+/**
+ * Input.
+ */
+
+ const getImage = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData)
+    document.getElementById("image").innerHTML =
+    '<img src="' + 
+      jsonData.message + 
+      '" alt="API Catgirl Image" ' +
+      '>'
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getImage("https://nekos.best/api/:version")
